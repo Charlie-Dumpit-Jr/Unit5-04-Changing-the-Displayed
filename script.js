f06aa0a49801c8abdf6a286fa6c49bd97b0594ac
CharlieDumpitJr <charlie.jr.dumpit@ocsbstudent.ca>
2021-03-17 15:00:58 +0000

    Unit5-04-Changing-the-Displayed (2)

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ document.getElementById('show').addEventListener('click', show)
 document.getElementById('background').addEventListener('click', background)
 
 function image () {
-  document.getElementById('currypicture').src="Images/wade.jpg"
+  document.getElementById('currypicture').src='Images/wade.jpg'
 }
 
 function text () {
-  document.getElementById('player').innerHTML = "This is Wade"
+  document.getElementById('player').innerHTML = 'This is Wade'
 }
 
 function hide () {
-  document.getElementById('player').style.display = "none"
+  document.getElementById('player').style.display = 'none'
 }
 
 function show () {
-  document.getElementById('player').style.display = "block"
+  document.getElementById('player').style.display = 'block'
 }
 
 function background () {
-  document.body.style.backgroundColor = "lightblue"
+  document.body.style.backgroundColor = 'lightblue'
 }
