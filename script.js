a48bd77a348e5abd8eaaae4f1f7d61f3dc33cd4d
CharlieDumpitJr <charlie.jr.dumpit@ocsbstudent.ca>
2021-03-17 15:13:23 +0000

    Unit5-04-Changing-the-Displayed (4)

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 
 document.getElementById('change-image').addEventListener('click', image)
-document.getElementById('change-text').addEventListener('click', text)
 document.getElementById('hide').addEventListener('click', hide)
 document.getElementById('show').addEventListener('click', show)
 document.getElementById('background').addEventListener('click', background)
 
 function image () {
   document.getElementById('currypicture').src = 'Images/wade.jpg'
-}
-
-function text () {
   document.getElementById('player').innerHTML = 'This is Wade'
 }
 
